cfaab2d19584e08e28ee4f1adb0d45fd2d05ab20
igorMSoares <igorms.dev@gmail.com>
2023-04-11 19:54:30 -0300

    create Button component

---
+++ components/Button.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  padding: 0.5rem 2rem;
+  background-color: #3867e2;
+  border-radius: 0.625rem;
+  border: 0;
+`;
+
+const Button = ({ type, label }) => {
+  return <StyledButton type={type}>{label}</StyledButton>;
+};
+
+Button.defaultProps = {
+  type: 'button',
+  label: 'Click Me',
+};
+
+export default Button;
