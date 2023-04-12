33929e2d0f0a223632a94890342017b2224d0035
igorMSoares <igorms.dev@gmail.com>
2023-04-12 00:15:48 -0300

    implement basic firebase auth logic

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,33 @@ import CardBox from '@/components/CardBox';
 import CardTitle from '@/components/CardTitle';
 import Button from '@/components/Button';
 import CentralizedContainer from '@/components/CentralizedContainer';
+import { auth } from '@/auth/firebase';
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
+
+const signIn = e => {
+  e.preventDefault();
+  signInWithEmailAndPassword(auth, '123@456.com', '123456')
+    .then(userCredentials => console.log(userCredentials))
+    .catch(err => console.log(err));
+};
+
+const signUp = e => {
+  e.preventDefault();
+  createUserWithEmailAndPassword(auth, '123@456.com', '123456')
+    .then(userCredentials => console.log(userCredentials))
+    .catch(err => console.log(err));
+};
 
 export default function Home() {
   return (
     <CentralizedContainer>
       <CardBox>
         <CardTitle title="Login" />
-        <Button label="Login" />
+        <Button label="Login" clickHandler={signIn} />
+        <Button label="Sign up" clickHandler={signUp} />
       </CardBox>
     </CentralizedContainer>
   );
