6f80a7fec3a35f09e2b14b9eae3ab9cba5d61997
adriano-aj <adrianobinan@gmail.com>
2023-04-12 15:10:54 -0300

    Organizei o diretório das pages

---
+++ pages/Login/Login.js
@@ -0,0 +1,42 @@
+import CardBox from '@/components/CardBox';
+import CardTitle from '@/components/CardTitle';
+import Button from '@/components/Button';
+import CentralizedContainer from '@/components/CentralizedContainer';
+import { auth, onAuthStateChanged } from '@/auth/firebase';
+import { handleSignIn, handleSignUp, handleSignOut } from '@/utils/auth';
+import { useEffect, useState } from 'react';
+
+const userEmail = 'user@someemail.com';
+const userPassword = 'userpasswd';
+
+export default function Login() {
+  const [currentUser, setCurrentUser] = useState(auth.currentUser);
+
+  useEffect(() => {
+    const removeListener = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+
+      return () => {
+        removeListener();
+      };
+    });
+  }, []);
+
+  return (
+    <CentralizedContainer>
+      <CardBox>
+        <CardTitle title="Login" />
+        <p>{currentUser?.email ?? 'Not logged'}</p>
+        <Button
+          label="Login"
+          clickHandler={e => handleSignIn({ userEmail, userPassword })}
+        />
+        <Button
+          label="Sign up"
+          clickHandler={e => handleSignUp({ userEmail, userPassword })}
+        />
+        <Button label="Sign Out" clickHandler={e => handleSignOut()} />
+      </CardBox>
+    </CentralizedContainer>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -5,38 +5,11 @@ import CentralizedContainer from '@/components/CentralizedContainer';
 import { auth, onAuthStateChanged } from '@/auth/firebase';
 import { handleSignIn, handleSignUp, handleSignOut } from '@/utils/auth';
 import { useEffect, useState } from 'react';
-
-const userEmail = 'user@someemail.com';
-const userPassword = 'userpasswd';
+import Login from './Login/login';
 
 export default function Home() {
-  const [currentUser, setCurrentUser] = useState(auth.currentUser);
-
-  useEffect(() => {
-    const removeListener = onAuthStateChanged(auth, user => {
-      setCurrentUser(user);
-
-      return () => {
-        removeListener();
-      };
-    });
-  }, []);
-
+  
   return (
-    <CentralizedContainer>
-      <CardBox>
-        <CardTitle title="Login" />
-        <p>{currentUser?.email ?? 'Not logged'}</p>
-        <Button
-          label="Login"
-          clickHandler={e => handleSignIn({ userEmail, userPassword })}
-        />
-        <Button
-          label="Sign up"
-          clickHandler={e => handleSignUp({ userEmail, userPassword })}
-        />
-        <Button label="Sign Out" clickHandler={e => handleSignOut()} />
-      </CardBox>
-    </CentralizedContainer>
+    <Login/>
   );
 }
