a404cbc08856e431708e2e51a681e74740def54c
VitorMesquitaAlves <130511691+VitorMesquitaAlves@users.noreply.github.com>
2023-04-13 00:04:07 -0300

    SplashScreen

--- pages/Splash/Splash.js
+++ pages/Splash/Splash.js
@@ -1,16 +1,36 @@
-import CardBox from '@/components/CardBox';
-import CardTitle from '@/components/CardTitle';
-import Button from '@/components/Button';
-import CentralizedContainer from '@/components/CentralizedContainer';
-import Form from '@/components/Form';
-import Text from '@/components/Text';
+import styles from '@/styles/splash.module.css';
+import Image from 'next/image';
+import profilerPic from '@/public/Killua.png'
+import Link from 'next/link';
 
-export default function Splash() {
+export default function splashScreen() {
+    return (
+        <div id={styles.div_background}>
+            <div id={styles.div_header}>
+                <div id={styles.div_menu}>
 
-  return (
-    <CentralizedContainer>
-      <CardBox>
-      </CardBox>
-    </CentralizedContainer>
-  );
+                    <div id={styles.div_logo}>
+                        <p id={styles.p_logo}> Sua Marca </p>
+                    </div>
+
+                    <div id={styles.div_buttons}>
+                        <Link id={styles.btn_login} href='/Login/Login'>
+                            <p className={styles.text_buttons}> Login </p>
+                        </Link>
+
+                        <Link id={styles.btn_cadastrar} href='/Register/Register'>
+                            <p className={styles.text_buttons}>Cadastrar-se</p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+            <div id={styles.div_marca}>
+                <div id={styles.div_logoIMG}>
+                    <Image src={profilerPic} width='300' height='300'/>
+                </div>
+                <p id={styles.tex_marca}>Sua Marca</p>
+            </div>
+        </div>
+    );
 }
